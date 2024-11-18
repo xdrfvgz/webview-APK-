7817fce84446750c9b27553d0064b4fb8f28116d
Christoph J. Lindner <xdrfvgz@gmail.com>
2024-11-18 10:15:51 +0000

    zweiter erster korrigier commit

--- app/src/main/java/com/webview/myapplication/MainActivity.java
+++ app/src/main/java/com/webview/myapplication/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity {
         });
 
         if (isNetworkAvailable()) {
-            mWebView.loadUrl("https://github.com/bishwassagar");
+            mWebView.loadUrl("http://localhost:5000");
         } else {
             mWebView.loadUrl("file:///android_asset/offline.html");
         }
@@ -59,7 +59,7 @@ public class MainActivity extends Activity {
             public void onAvailable(Network network) {
                 runOnUiThread(() -> {
                     if (!mWebView.getUrl().startsWith("file:///android_asset")) {
-                        mWebView.loadUrl("https://github.com/bishwassagar");
+                        mWebView.loadUrl("http://localhost:5000");
                     }
                 });
             }
